4459af92e84a19cd262126dbc20b007dfe0eecbe
Chelsi Ampawa <chelsiampawa@gmail.com>
2018-01-14 16:41:37 -0500

    ok

--- app/src/main/java/com/example/chelsi/kcjhomework/MainActivity.java
+++ app/src/main/java/com/example/chelsi/kcjhomework/MainActivity.java
@@ -26,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler);
         layoutManager = new LinearLayoutManager(getApplicationContext());
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create().build());
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
     }
 }
